610c90276305e354c78a982c39459077c386554c
neophoenix733 <gawad62984@gmail.com>
2017-10-25 12:41:08 -0700

    audio dependencies added, not met

---
+++ js/audio.js
@@ -0,0 +1,16 @@
+// variable to store HTML5 audio element
+var music = document.getElementById('audio_player');
+ 
+function playAudio() {
+  if (music.paused) {
+    music.play();
+    pButton.className = "";
+    pButton.className = "pause";
+  } else {
+    music.pause();
+    pButton.className = "";
+    pButton.className = "play";
+  }
+}
+
+
